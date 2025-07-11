cec1d87642476489816026cca1486d5c3af5c11e
Divanshu Sharma <139094815+div0506@users.noreply.github.com>
2025-07-11 13:28:24 +0530

    Update model_loader.py

--- airbnb_price_prediction_app/model_loader.py
+++ airbnb_price_prediction_app/model_loader.py
@@ -1,11 +1,35 @@
 import gdown
 import pickle
+import os
 
 def load_model():
-    url = "https://drive.google.com/uc?id=1zpa4zHZOm-Hk3zkUHKEGy-oUrmbX1nq5"
-    output = "model.pkl"
-    gdown.download(url, output, quiet=False)
-    
-    with open(output, "rb") as f:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Paths
+    model_path = os.path.join(base_dir, "model.pkl")
+    columns_path = os.path.join(base_dir, "model_columns.pkl")
+
+    # Download model if not present
+    if not os.path.exists(model_path):
+        gdown.download(
+            "https://drive.google.com/uc?id=1zpa4zHZOm-Hk3zkUHKEGy-oUrmbX1nq5",
+            model_path,
+            quiet=False
+        )
+
+    # Download columns if not present
+    if not os.path.exists(columns_path):
+        gdown.download(
+            "https://drive.google.com/uc?id=144QNun-eO1Tu0ZgE2POnN4rCe_c5nWxJ",
+            columns_path,
+            quiet=False
+        )
+
+    # Load both
+    with open(model_path, "rb") as f:
         model = pickle.load(f)
-    return model
+
+    with open(columns_path, "rb") as f:
+        model_columns = pickle.load(f)
+
+    return model, model_columns
